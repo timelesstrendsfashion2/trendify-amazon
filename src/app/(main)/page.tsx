774fe8fa8ad13997da9c574aa35fbba595a16e16
Hanzlah Javaid <hanzlahjavaid@Hanzlahs-MacBook-Air.local>
2024-05-05 12:25:45 +0500

    changed styling

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -31,7 +31,7 @@ export default async function HomePage() {
 
       <div className="py-12 max-w-7xl mx-auto px-6 space-y-8">
         <h1 className="text-3xl font-bold">Featured Products</h1>
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 gap-6">
           {products.map((p) => (
             <ProductCard key={p.id} product={p} />
           ))}
--- src/app/(main)/search/page.tsx
+++ src/app/(main)/search/page.tsx
@@ -54,7 +54,7 @@ export default async function SearchPage({
         </div>
       )}
 
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 gap-6">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
